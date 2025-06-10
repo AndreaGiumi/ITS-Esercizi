a6588eaf228f5ce41ae52ad7f436fdc2f0d7f11a
AndreaGiumi <andreagiumi97@gmail.com>
2025-06-10 07:50:33 +0200

    esercizi recupero

--- Esercizio Python/Esercizi_Recupero/esercizio_9.py
+++ Esercizio Python/Esercizi_Recupero/esercizio_9.py
@@ -2,11 +2,12 @@ from string import ascii_lowercase, ascii_uppercase
 
 def caesar_cypher_encrypt(s:str, key:int):
     lettere: list[str] = ascii_lowercase
-    lettere_upp: list[str] = ascii_uppercase
-
-    for i in lettere:
-        return i
     for l in s:
-        return l
-
-caesar_cypher_encrypt("ci",4)
+        if l in lettere:
+            index_l = lettere.index(l) + key
+            print(f"{l}, {index_l}")
+        if key > index_l:
+            mod = index_l % key
+            print(mod)
+                
+caesar_cypher_encrypt("ci", 33)
